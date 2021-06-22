bfc9a3eb5836d45e09062d7a1b8364d3b90210f6
sjh4773 <jihun950202@naver.com>
2021-06-22 23:19:21 +0900

    HackerRank 문제풀이 06_20

--- HackerRank_Practice.sql
+++ HackerRank_Practice.sql
@@ -68,10 +68,11 @@ SELECT C.company_code
      , COUNT(DISTINCT SM.senior_manager_code)
      , COUNT(DISTINCT M.manager_code)
      , COUNT(DISTINCT E.employee_code)
-FROM Company c
+FROM Company C
      LEFT JOIN Lead_Manager LM ON C.company_code = LM.company_code
      LEFT JOIN Senior_Manager SM ON LM.lead_manager_code = SM.lead_manager_code
      LEFT JOIN Manager M ON SM.senior_manager_code = M.senior_manager_code
      LEFT JOIN Employee E ON M.manager_code = E.manager_code
-GROUP BY
+GROUP BY C.company_code, C.founder
+ORDER BY C.company_code
 
