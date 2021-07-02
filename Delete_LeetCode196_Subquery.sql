599f9bc3929b2febb6dfe2997172a882f039c295
sjh4773 <jihun950202@naver.com>
2021-07-02 20:59:48 +0900

    DELETE 문제풀이 + 서브쿼리 + DELETE with INNER JOIN

--- Delete_LeetCode196_Subquery.sql
+++ Delete_LeetCode196_Subquery.sql
@@ -60,3 +60,57 @@ SELECT Email, MIN(Id) AS min_id
 FROM Person
 GROUP BY Email
 ) sub )
+
+
+-- [심화] DELETE에 JOIN 활용하기
+
+https://www.mysqltutorial.org/mysql-delete-join/
+
+DELETE t1, t2
+FROM t1
+INNER JOIN t2 ON t1.id = t2.ref
+WHERE t1.id = 1;
+
+-- 원래 데이터
+t1     t2               
+
+Id     id ref
+1      A  1
+2      B  2
+3      C  3
+
+-- DELETE 적용
+t1     t2
+
+Id     id ref
+2      B  2
+3      C  3
+
+DELETE p1
+SELECT
+FROM Person p1
+     INNER JOIN Person p2 ON p1.Email = p2.Email
+WHERE p1.Id > p2.Id
+
+
+
+쿼리는 p1.Id 가 p2.Id 보다 큰 경우,
+그러니까 같은 이메일을 가지지만 Id가 다른 데이터보다 큰 로우들을 찾아 삭제합니다.
+
+중요한 포인트는 같은 이메일이 반복되면, 제일 작은 Id 이외의 다른 Id를 찾아주고 이 값을 삭제해야하는 것입니다. 
+
+테이블을 Email을 기준으로 조인하게 된다면 아래처럼 보여지는데, 
+
+p1.Id    p1.Email                          p2.Id    p2.Email
+1            john@example.com   1            john@example.com
+1            john@example.com   3            john@example.com
+2            bob@example.com    2            bob@example.com
+3            john@example.com   1            john@example.com
+3           john@example.com    3            john@example.com
+
+위에 p1와 p2를 조인한 결과에서 WHERE p1.Id > p2.Id 로 필터링을 하게 되면 아래와 같은 결과가 나오고,
+
+p1.Id    p1.Email                          p2.Id    p2.Email
+3            john@example.com   1            john@example.com 
+
+DELETE는 p1을 기준으로 하고 있기 때문에 Person 테이블에서 Id가 3번인 데이터를 찾아 지웁니다.
